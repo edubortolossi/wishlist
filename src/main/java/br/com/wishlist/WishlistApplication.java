e2aa0254e07d048a845b9e9889900c0a8cf6f601
Eduardo Bortolossi <ececotti.vermont@vstsalelo.onmicrosoft.com>
2021-04-02 09:14:19 -0300

    Criação do application

---
+++ src/main/java/br/com/wishlist/WishlistApplication.java
@@ -0,0 +1,9 @@
+package br.com.wishlist;
+
+import org.springframework.boot.SpringApplication;
+
+public class WishlistApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( WishlistApplication.class, args );
+    }
+}
